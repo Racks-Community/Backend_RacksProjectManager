b3596e6f188b935613740fed7a5e4274d5eea76a
KaladinStormBlessed <davidcr.odalys@gmail.com>
2022-10-24 01:26:40 +0200

    .env.example cleanup

--- app/middleware/auth/index.js
+++ app/middleware/auth/index.js
@@ -2,7 +2,6 @@ const { checkPassword } = require('./checkPassword')
 const { decrypt } = require('./decrypt')
 const { encrypt } = require('./encrypt')
 const { getNfts } = require('./getNfts')
-const { mintTest } = require('./mintTest')
 const { validateNftsAddress } = require('./validateNftsAddress')
 const { validateHolder, validateHolderInternal } = require('./validateHolder')
 
@@ -13,6 +12,5 @@ module.exports = {
   validateHolder,
   validateHolderInternal,
   getNfts,
-  mintTest,
   validateNftsAddress
 }
